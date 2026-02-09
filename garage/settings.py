23c66b472c70a13fe58e023ac31cbdeb4e26a58b
AfricomTZ <fricomenergies@gmail.com>
2026-02-09 16:53:27 +0300

    Dockerize the settings

--- garage/settings.py
+++ garage/settings.py
@@ -87,31 +87,31 @@ WSGI_APPLICATION = 'garage.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'garage_db',
-        'USER': 'root', 
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '3306',   #my port is 3306
-    }
-}
-
-# import os
 # DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.mysql",
-#         "NAME": os.getenv("DB_NAME"),
-#         "USER": os.getenv("DB_USER"),
-#         "PASSWORD": os.getenv("DB_PASSWORD"),
-#         "HOST": os.getenv("DB_HOST"),
-#         "PORT": "3306",
-#         "OPTIONS": {
-#             "init_command": "SET sql_mode='STRICT_TRANS_TABLES'"
-#         }
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'garage_db',
+#         'USER': 'root', 
+#         'PASSWORD': '',
+#         'HOST': '',
+#         'PORT': '3306',   #my port is 3306
 #     }
 # }
+
+import os
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": "3306",
+        "OPTIONS": {
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
+    }
+}
 
 
 # Password validation
