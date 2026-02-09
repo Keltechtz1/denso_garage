19402d400e736a2b5aba5c3ee609b612377dd982
AfricomTZ <fricomenergies@gmail.com>
2026-02-09 19:22:18 +0300

    adding allowed host

--- garage/settings.py
+++ garage/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-r&dt*cnp1dngw(_1lg!%yc^6gqrew%@a$3q6_*22lcfj4^z&1x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['densomotorsgarage.com', 'www.densomotorsgarage.com']
 # ALLOWED_HOSTS = ['www.mg.densomotors.com','mg.densomotors.com']
 
 
